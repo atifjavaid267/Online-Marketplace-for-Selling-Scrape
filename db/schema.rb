944f3d970828ba822d5e7ed2d98beba876ec23e5
Atif <atifjavaid267@gmail.com>
2023-03-20 12:03:18 +0500

    users database modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_135815) do
+ActiveRecord::Schema.define(version: 2023_03_20_064753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,12 @@ ActiveRecord::Schema.define(version: 2023_03_17_135815) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_no"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_no"], name: "index_users_on_phone_no", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
