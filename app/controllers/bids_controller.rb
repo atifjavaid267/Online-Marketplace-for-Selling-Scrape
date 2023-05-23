13253a9198af4504db0ffa11229da522e0616316
Atif <atifjavaid267@gmail.com>
2023-05-23 18:58:19 +0500

    refactored code

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -4,10 +4,9 @@
 class BidsController < ApplicationController
   load_and_authorize_resource
   before_action :authenticate_user!
-  before_action :store_location, only: %i[new]
+  before_action :store_location, only: %i[new show]
 
   def new
-    @bid.user_id = current_user.id
     @bid.ad_id = params[:ad_id]
   end
 
@@ -21,7 +20,7 @@ class BidsController < ApplicationController
                                      { ad_id: @bid.ad_id, price: @bid.price, buyer_id: @bid.user_id,
                                        buyer_name: @bid.user.first_name })
         format.json { render :show, status: :created, location: @bid }
-        format.html { redirect_to users_home_path }
+        format.html { redirect_to ads_path }
       end
     else
       flash[:alert] = @bid.errors.full_messages.join(', ')
