dc8305c1fffd44d4e757d457b79fe4b67d3726a5
Atif <atifjavaid267@gmail.com>
2023-06-01 02:36:48 +0500

    extra code removed

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,125 +1,24 @@
 import consumer from "./consumer";
 
-// document.addEventListener("turbolinks:load", () => {
-//   const userDiv = document.getElementById("user");
-//   const current_user_id = userDiv.getAttribute("data-user-id");
-//   const countElement = document.getElementById("notification-count");
-//   const notificationDropdown = document.getElementById("notification-dropdown");
-
-//   const storedCount = localStorage.getItem("count") || 0;
-//   countElement.innerHTML = storedCount;
-
-//   const storedNotifications =
-//     JSON.parse(localStorage.getItem("notifications")) || {};
-//   const messageCounts = storedNotifications.messageCounts || {};
-//   const notificationMessages = storedNotifications.notificationMessages || {};
-
-//   for (const pairId in messageCounts) {
-//     const senderId = pairId.split("-")[0];
-//     const senderName = notificationMessages[pairId].senderName;
-//     const messageCount = messageCounts[pairId];
-//     const orderID = notificationMessages[pairId].orderID;
-
-//     const notificationMessage = document.createElement("div");
-//     notificationMessage.textContent = `${senderName} sent you ${messageCount} messages`;
-//     notificationMessage.classList.add("p-2", "text-black");
-//     notificationMessage.id = `notification-${pairId}`;
-//     notificationMessage.addEventListener("click", () => {
-//       const messageURL = `/orders/${orderID}/messages/new`;
-//       window.location.href = messageURL;
-//     });
-//     notificationDropdown.appendChild(notificationMessage);
-//   }
-
-//   const handleNotification = (data) => {
-//     console.log(data);
-//     const senderId = data.sender_id;
-//     const receiverId = data.receiver_id;
-//     const senderName = data.sender_name;
-//     const orderID = data.order_id;
-
-//     const pairId = `${senderId}-${receiverId}`;
-//     const existingMessage = document.getElementById(`notification-${pairId}`);
-
-//     if (existingMessage) {
-//       const messageCount = messageCounts[pairId];
-//       existingMessage.textContent = `${senderName} sent you ${
-//         messageCount + 1
-//       } messages`;
-//       messageCounts[pairId] += 1;
-//     } else {
-//       const notificationMessage = document.createElement("div");
-//       notificationMessage.textContent = `${senderName} sent you a message`;
-//       notificationMessage.classList.add("p-2", "text-black");
-//       notificationMessage.id = `notification-${pairId}`;
-//       notificationMessage.addEventListener("click", () => {
-//         const messageURL = `/orders/${orderID}/messages/new`;
-
-//         window.location.href = messageURL;
-//       });
-//       notificationDropdown.appendChild(notificationMessage);
-//       messageCounts[pairId] = 1;
-//     }
-
-//     notificationMessages[pairId] = {
-//       senderName,
-//       messageCount: messageCounts[pairId],
-//       orderID,
-//     };
-
-//     const notifications = {
-//       messageCounts,
-//       notificationMessages,
-//     };
-//     localStorage.setItem("notifications", JSON.stringify(notifications));
-
-//     const newCount = Object.values(messageCounts).reduce(
-//       (total, count) => total + count,
-//       0
-//     );
-//     localStorage.setItem("count", newCount);
-//     countElement.innerHTML = newCount;
-//   };
-
-//   consumer.subscriptions.create(
-//     { channel: "NotificationChannel", user_id: current_user_id },
-//     {
-//       connected() {
-//         console.log(`Connected to the NotificationsChannel ${current_user_id}`);
-//       },
-//       disconnected() {},
-//       received(data) {
-//         handleNotification(data);
-//       },
-//     }
-//   );
-// });
-
 document.addEventListener("turbolinks:load", () => {
   const userDiv = document.getElementById("user");
   const current_user_id = userDiv.getAttribute("data-user-id");
   const countElement = document.getElementById("notification-count");
   const notificationDropdown = document.getElementById("notification-dropdown");
-
   const storedCount = localStorage.getItem("count") || 0;
   countElement.innerHTML = storedCount;
-
-  const storedNotifications =
-    JSON.parse(localStorage.getItem("notifications")) || {};
+  const storedNotifications = JSON.parse(localStorage.getItem("notifications")) || {};
   const messageCounts = storedNotifications.messageCounts || {};
   const notificationMessages = storedNotifications.notificationMessages || {};
 
-  // Event listener for message new page
   if (window.location.pathname.includes("/messages/new")) {
     localStorage.setItem("count", "0");
     countElement.innerHTML = "0";
     notificationDropdown.innerHTML = "";
-    localStorage.setItem(
-      "notifications",
-      JSON.stringify({ messageCounts: {}, notificationMessages: {} })
+    localStorage.setItem("notifications", JSON.stringify({ messageCounts: {}, notificationMessages: {} })
     );
-  } else {
-    // Render existing notification messages
+  }
+  else {
     for (const pairId in messageCounts) {
       const senderId = pairId.split("-")[0];
       const senderName = notificationMessages[pairId].senderName;
@@ -180,10 +79,7 @@ document.addEventListener("turbolinks:load", () => {
     };
     localStorage.setItem("notifications", JSON.stringify(notifications));
 
-    const newCount = Object.values(messageCounts).reduce(
-      (total, count) => total + count,
-      0
-    );
+    const newCount = Object.values(messageCounts).reduce((total, count) => total + count, 0);
     localStorage.setItem("count", newCount);
     countElement.innerHTML = newCount;
   };
