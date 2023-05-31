12ade7aea6e482f2ee3568ad4e03e8d2f51de7f9
Atif <atifjavaid267@gmail.com>
2023-05-31 16:43:40 +0500

    minoe changes

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,10 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 require 'active_storage/attached'
 require 'active_storage/engine'
-
 require 'will_paginate/array'
 
-WillPaginate.per_page = RECORDS_PER_PAGE
+WillPaginate.per_page = 10
 
 ActiveSupport.on_load(:active_record) do
   include ActiveStorage::Reflection::ActiveRecordExtensions
@@ -15,23 +12,11 @@ ActiveSupport.on_load(:active_record) do
   include ActiveStorage::Attached::Model
 end
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module OnlineSellingScrape
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    # config/application.rb
-    # config.middleware.use ActionCable.server
-    # config.action_controller.default_protect_from_forgery = true
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
     config.time_zone = 'Karachi'
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
