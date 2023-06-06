d07393c01a539086de837aa0e0b1d5388cc12853
Atif <atifjavaid267@gmail.com>
2023-06-06 16:17:21 +0500

    helper refactored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
-module BidsHelper
-  def bids_status_color(status)
+module ApplicationHelper
+  def status_color(status)
     case status
     when 'pending'
       'text-blue-600'
     when 'successful'
       'text-green-600'
-    when 'failed'
+    when 'failed', 'cancelled'
       'text-red-600'
     else
       ''
