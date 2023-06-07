b1600635d0e5e1f4776aabf1be8b4338dcee33d2
kamil7862 <kamilhussain980@gmail.com>
2023-06-07 18:30:19 +0500

    view_bids message refactored

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,10 +10,6 @@ class Message < ApplicationRecord
   validates :sender_id, presence: true
   validates :receiver_id, presence: true
 
-  def self.unread_message_count(sender_id, receiver_id)
-    where(receiver_id:, sender_id:, read_at: nil).count
-  end
-
   def self.messages(current_user, sender_id)
     where(sender_id: [current_user, sender_id], receiver_id: [current_user, sender_id])
   end
