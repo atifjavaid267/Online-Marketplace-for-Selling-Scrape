521d55e6ababedd84d97500a80fd8bb743012258
Atif <atifjavaid267@gmail.com>
2023-06-12 13:25:15 +0500

    unused code removed

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,10 +8,4 @@ class Notification < ApplicationRecord
   def increment_total
     update(total: total + 1)
   end
-
-  # def self.clear_notifications(current_user_id, buyer_id, seller_id)
-  #   sender_id = current_user_id == seller_id ? buyer_id : seller_id
-  #   notification = find_by(receiver_id: current_user_id, sender_id:)
-  #   notification.update(total: 0) if notification && notification.total > 0
-  # end
 end
